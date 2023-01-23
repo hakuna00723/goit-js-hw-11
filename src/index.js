e310f72f1b6bdf043c7dea23af65755dd36bfdc4
Serhii <110534352+hakuna00723@users.noreply.github.com>
2023-01-23 21:55:50 +0100

    add markup

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ console.log(axios);
 
 import Notiflix from 'notiflix';
 console.log(Notiflix);
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  btnLoadMore: document.querySelector('.load-more'),
+};
